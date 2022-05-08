4b8d60cc171f169466aabea80d4924c9b52e1498
Sivaram <s3sivaram@gmail.com>
2022-05-08 16:14:59 +0530

    Added Brackets.js

--- general/brackets.js
+++ general/brackets.js
@@ -1,1 +1,24 @@
-// testing the brackets file
+// return true if the pairs of the brackets in the string is in correct order
+// stacklength-2 and stacklength-1 will give the corresponding closing & opening brackets.
+
+let fixedpairs = ["{}", "()", "[]"];
+let input = "[{[{}]}]";
+// let input = "[]";
+let inputarray = [];
+let pointer = 0;
+while (pointer < input.length) {
+  inputarray.push(input.charAt(pointer));
+  let leftbracket = inputarray[inputarray.length - 2];
+  let rightbracket = inputarray[inputarray.length - 1];
+  let formedpairs = leftbracket + rightbracket;
+  if (fixedpairs.includes(formedpairs)) {
+    inputarray.pop();
+    inputarray.pop();
+  }
+  pointer++;
+}
+if (inputarray.length) {
+  console.log(`${input} does not have matching brackets`);
+} else {
+  console.log(`${input} has matching brackets`);
+}
